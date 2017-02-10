e5e1913046cbb5542a017b103cfc3acbf7791f77
Xcode User <xcode@fasts-macbook-pro-6.local>
2017-02-09 19:40:27 -0500

    Initial Commit

---
+++ Assignment1/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Assignment1
+//
+//  Created by Xcode User on 2017-02-09.
+//  Copyright © 2017 Xcode User. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Assignment1/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Assignment1
+//
+//  Created by Xcode User on 2017-02-09.
+//  Copyright © 2017 Xcode User. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
